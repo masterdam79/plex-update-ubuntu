3a09027184139e96412d5705e24daafb265ae9e3
Richard Reijmers <rreijmers@denit.net>
2016-04-25 05:22:49 +0200

    Version check

--- getlatestandinstall.sh
+++ getlatestandinstall.sh
@@ -3,8 +3,10 @@
 # Include
 source functions.sh
 
-# Nice clean URL
+# Nice clean URL, file & version
 DEBURL=$(curl -s https://plex.tv/downloads | grep amd64.deb | grep -v binaries | sed 's/\s.*<a href="//g' | sed 's/\.deb.*<\/a>/.deb/g')
+DEBFILE=$(echo ${DEBURL} | awk -F'/' '{print $6}')
+DEBVERSION=$(echo ${DEBFILE} | awk -F'_' '{print $2}')
 
 # Current Plex media server version if installed
 PLEXVERSION=$(dpkg -s plexmediaserver | grep Version | awk '{print $2}')
@@ -19,5 +21,24 @@ ECHOSILVER "${DEBURL}"
 ASKFORIT PROCEED "Proceed? (y/n): "
 
 if [[ ${PROCEED} == "y" ]]; then
-  #statements
+  if [[ ${PLEXVERSION} == "" ]]; then
+    # Plex media server is not installed
+    ECHOGREEN "Installing Plex Media Server"
+  else
+    # Plex media server is installed
+    ECHOGREEN "Updating Plex Media Server"
+    VERSIONCHECK=$(echo ${DEBURL} | grep ${PLEXVERSION})
+    if [[ ${VERSIONCHECK} == "" ]]; then
+      # Currently installed Plex Media Server is a different version than the one available for download, updating.
+      ECHOGREEN "Currently installed Plex Media Server is a different version than the one available for download, updating."
+      ECHOSILVER "Current version: ${PLEXVERSION}"
+      echo "Current version: ${DEBVERSION}"
+      wget ${DEBURL} -O /root/${DEBFILE}
+      dpkg -i /root/${DEBFILE}
+    else
+      # Currently installed Plex Media Server is the same version, no update needed.
+      ECHORED "Currently installed Plex Media Server is the same version, no update needed."
+    fi
+  fi
+
 fi
