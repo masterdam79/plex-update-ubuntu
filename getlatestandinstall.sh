74ed8cd41f20c06ecac413ec95563986e9da14bc
Richard Reijmers <rreijmers@denit.net>
2016-04-25 04:48:18 +0200

    more verbosity

--- getlatestandinstall.sh
+++ getlatestandinstall.sh
@@ -6,6 +6,14 @@ source functions.sh
 # Nice clean URL
 DEBURL=$(curl -s https://plex.tv/downloads | grep amd64.deb | grep -v binaries | sed 's/\s.*<a href="//g' | sed 's/\.deb.*<\/a>/.deb/g')
 
+# Current Plex media server version if installed
+PLEXVERSION=$(dpkg -s plexmediaserver | grep Version | awk '{print $2}')
+
+if [[ ${PLEXVERSION} == "" ]]; then
+  # Plex media server is not installed
+  ECHORED "Plex media server is not installed"
+fi
+
 ECHOYELLOW "We'll download the newest version of Plex from this URL: "
 ECHOSILVER "${DEBURL}"
 ASKFORIT PROCEED "Proceed?"
